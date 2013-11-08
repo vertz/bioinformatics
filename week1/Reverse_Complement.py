d61cc34a9ef6383c5eff14f2bb899b8fe3632f65
vertz <winter333@gmail.com>
2013-11-08 18:02:10 +0200

    Update Reverse_Complement.py

--- week1/Reverse_Complement.py
+++ week1/Reverse_Complement.py
@@ -1,8 +1,8 @@
 ##################################
 #
 # Reverse Complement Problem: Reverse complement a nucleotide pattern.
-#	Input: A DNA string Pattern.
-# 	Output: @Pattern, the reverse complement of Pattern.
+#    Input: A DNA string Pattern.
+#    Output: @Pattern, the reverse complement of Pattern.
 #
 ################################## 
 
@@ -15,24 +15,24 @@ def complement(nuc):
     }.get(nuc, "")
 
 def reverseString(s):
-	return s[::-1]
+    return s[::-1]
 
 def solve(dna):
-	cdna = ""
+    cdna = ""
 
-	for nuc in dna:
-		cdna += complement(nuc)
+    for nuc in dna:
+        cdna += complement(nuc)
 
-	f = open('output.txt', 'w')
-	f.write(reverseString(cdna) + '\n')
-	f.close()
+    f = open('output.txt', 'w')
+    f.write(reverseString(cdna) + '\n')
+    f.close()
 
 def main():
-	f = open('Input.txt', 'r')
-	dna = f.readline()
-	f.close()
+    f = open('Input.txt', 'r')
+    dna = f.readline()
+    f.close()
 
-	solve(dna)
+    solve(dna)
     
 if __name__ == '__main__':
     main()
